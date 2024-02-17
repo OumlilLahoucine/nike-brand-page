79d41bf05d3fe2af6d81d691a60f32f933d94d0e
oumlillahoucine <O.Lahoucine16@gmail.com>
2024-02-17 16:15:48 +0000

    Upload 'nike brand page' project to Github

---
+++ README.md
@@ -0,0 +1,52 @@
+# Nike - brand page
+
+This is the front-end code of the [Nike brand page](https://www.figma.com/file/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?type=design&node-id=0-1&t=ExzjQLThD1EGWh8Q-0) design.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+The challenge is to create the [Nike brand page](mylink) and make it look as close to this [design](https://www.figma.com/file/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?type=design&node-id=0-1&t=ExzjQLThD1EGWh8Q-0).
+
+### Screenshot
+
+Desktop screen:
+![](./design/screenshot.png)
+
+Mobile screen:
+![](./design/screenshot.png)
+
+### Links
+
+- Solution URL: [Nike-brand-design-code](https://github.com/OumlilLahoucine/Frontend-Mentor--Recipe-page)
+- Live Site URL: [Nike-brand-design-code](https://oumlillahoucine.github.io/Frontend-Mentor--Recipe-page/)
+
+<!-- ## My process -->
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Css Grid
+- ReactJs
+- Desktop-first workflow
+
+### Useful resources
+
+- I personally really like the Udemy courses of 'Jonas Schmedtmann' (specially, [Build Responsive Real-World Websites with HTML and CSS](https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/) & [The Ultimate React Course 2024: React, Redux & More](https://www.udemy.com/course/the-ultimate-react-course/))
+
+## Author
+
+- Github - [Oumlil Lahoucine](https://github.com/oumlillahoucine)
+- Frontend Mentor - [@oumlillahoucine](https://www.frontendmentor.io/profile/OumlilLahoucine)
---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+function Header() {
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <header className={`header ${navOpen ? "nav-open" : ""}`}>
+      <picture>
+        <source srcSet="/images/logo.webp" type="image/webp" />
+        <source srcSet="/images/logo.png" type="image/png" />
+        <img src="/images/logo.png" alt="Nike logo" className="img-logo" />
+      </picture>
+
+      <nav className="navigation">
+        <Link text="Menu" onClick={() => setNavOpen(false)} />
+        <Link text="Location" onClick={() => setNavOpen(false)} />
+        <Link text="About" onClick={() => setNavOpen(false)} />
+        <Link text="Contact" onClick={() => setNavOpen(false)} />
+        <Button text="Login" type="login" onClick={() => setNavOpen(false)} />
+      </nav>
+      <Button text="Login" type="login" />
+
+      <button
+        aria-label="Open and close navbar"
+        className="btn-menu"
+        onClick={() => setNavOpen((cur) => !cur)}
+      >
+        <ion-icon
+          name={`${navOpen ? "close-outline" : "menu-outline"}`}
+        ></ion-icon>
+      </button>
+    </header>
+  );
+}
+
+function Hero() {
+  return (
+    <main className="container hero">
+      <TextBox />
+      <ImageBox />
+    </main>
+  );
+}
+
+function TextBox() {
+  return (
+    <div className="text-box">
+      <h1 className="primary-headline">Your feet deserve the best</h1>
+      <p className="description">
+        Your feet deserve the best and we’re here to help you with our
+        shoes.your feet deserve the best and we’re here to help you with our
+        shoes.
+      </p>
+      <div className="buttons">
+        <Button text="Shop now" />
+        <Button text="Category" type="secondary" />
+      </div>
+      <div className="available">
+        <span>Also available on</span>
+        <ul>
+          <Market imageName="etsy.png" alt="Etsy site" />
+          <Market imageName="amazon.png" alt="Amazon site" />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function ImageBox() {
+  return (
+    <div className="image-box">
+      <picture>
+        <source srcSet="/images/domino-cropped.webp" type="image/webp" />
+        <source srcSet="/images/domino-cropped.png" type="image/png" />
+        <img
+          src="/images/domino-cropped.png"
+          alt="Nike domino"
+          className="domino-img"
+        />
+      </picture>
+    </div>
+  );
+}
+
+function Button({ type = "primary", text, href = "#", onClick = {} }) {
+  let style = "";
+  if (type === "primary") {
+    style = "btn-shop";
+  } else if (type === "secondary") {
+    style = "btn-category";
+  } else {
+    style = "btn-nav";
+  }
+  return (
+    <a
+      href={href}
+      className={`btn ${style}`}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {text}
+    </a>
+  );
+}
+
+function Market({ imageName, alt = "Image", href = "#" }) {
+  return (
+    <li>
+      <a href={href}>
+        <img src={`/images/${imageName}`} alt={alt} className="market-img" />
+      </a>
+    </li>
+  );
+}
+
+function Link({ href = "#", text, onClick }) {
+  return (
+    <a
+      href={href}
+      className="nav-link"
+      onClick={(e) => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {text}
+    </a>
+  );
+}
